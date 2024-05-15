982feb388e84255bc9340cd4a8063d0e4bbfd994
Steve Jonk <33755008+SteveJonk@users.noreply.github.com>
2024-05-15 22:05:12 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ rm ~/.zshrc
 rm ~/.vimrc
 
 # Symlinks to dotfiles
-ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/git/.gitconfig-labdigital ~/.gitconfig-labdigital
-ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitconfig-labdigital ~/.gitconfig-labdigital
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
